5c3239e58749c037e3882c7f2a118fcb7d7e9499
Denis Skiba <dskiba@docdoc.ru>
2019-10-13 16:56:47 +0400

    add alphanumeric.js kata

--- src/alphanumeric.test.js
+++ src/alphanumeric.test.js
@@ -1,34 +1,21 @@
 import { alphanumeric } from './alphanumeric';
 
-/*
- * Example of the assertions you can use for your tests:
- *
- * - Testing equality between the function's output and a constant:
- * expect(alphanumeric()).toEqual(123);
- *
- * - Testing that the function returns null:
- * expect(alphanumeric()).toBeNull();
- *
- * - Testing that the function returns a falsy value (eg. false, 0, "")
- * expect(alphanumeric()).toBeFalsy();
- *
- * - Testing that the function returns a truthy value (eg. true, 1, "abc")
- * expect(alphanumeric()).toBeTruthy();
- *
- * - Testing that the function throws
- * expect(() => { alphanumeric(); }).toThrow();
- */
-
 describe('alphanumeric', () => {
   it('alphanumeric does this thing...', () => {
-    // TODO Your own assertion here
+    expect(alphanumeric('')).toEqual(false)
+  });
+  it('alphanumeric underscore.', () => {
+    expect(alphanumeric('hello world_')).toEqual(false)
+  });
+  it('alphanumeric test empty string\'    \'', () => {
+    expect(alphanumeric('     ')).toEqual(false)
   });
 
-  it('alphanumeric does that other thing...', () => {
-    // TODO Your own assertion here
+  it('alphanumeric passw0rd.', () => {
+    expect(alphanumeric('PassW0rd')).toEqual(true)
   });
 
-  it('alphanumeric does a very cool thing...', () => {
-    // TODO Your own assertion here
+  it('alphanumeric empty string to false', () => {
+    expect(alphanumeric('')).toEqual(false)
   });
 });
