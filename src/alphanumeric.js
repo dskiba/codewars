45a2c3f7041f31411588ca4e0c8554810c9ca958
Den Skiba <skdenx@gmail.com>
2020-05-02 21:40:21 +0400

    add didYouMean

--- src/alphanumeric.js
+++ src/alphanumeric.js
@@ -1,4 +1,4 @@
-export const alphanumeric = (string) => /^[a-z\d]+$/i.test(string);
+export const alphanumeric = (string) => /^[a-z\d]+$/i.test(string)
 
 
 // In this example you have to validate if a user input string is alphanumeric.
@@ -9,3 +9,23 @@ export const alphanumeric = (string) => /^[a-z\d]+$/i.test(string);
 //     At least one character ("" is not valid)
 // Allowed characters are uppercase / lowercase latin letters and digits from 0 to 9
 // No whitespaces / underscore
+
+
+const solution = function (isBadVersion) {
+  /**
+     * @param {integer} n Total versions
+     * @return {integer} The first bad version
+     */
+  return function (n) {
+    let res = null
+    for (let i = n; i > 0; i--) {
+      if (!isBadVersion(i)) {
+            i > 1 ? res=i+1 : res = 1
+      }
+    }
+  return res
+};
+
+const isBadVersionF = (n) => n === 4
+
+solution(isBadVersionF)(5)
