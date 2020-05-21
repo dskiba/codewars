4b9e8f7c621627dc61cfa000c333411ca1975122
Den Skiba <skdenx@gmail.com>
2020-05-21 22:05:30 +0400

    add parseInt init

---
+++ src/parseInt/parseInt.js
@@ -0,0 +1,3 @@
+export const parseInt = () => {
+  // TO IMPLEMENT IN ANOTHER PR
+};
---
+++ src/parseInt/parseInt.test.js
@@ -0,0 +1,9 @@
+import { parseInt } from './parseInt';
+
+describe('parseInt', () => {
+  it('parseInt does this thing...', () => {
+    const input = null
+    const res = null
+    expect(parseInt(input)).toEqual(res)
+  });
+});
