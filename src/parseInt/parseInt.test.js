da22959cedb251448e6a5c551e2771ff5f83e8a3
Den Skiba <skdenx@gmail.com>
2020-05-21 22:17:14 +0400

    parseInt top solution

--- src/parseInt/parseInt.test.js
+++ src/parseInt/parseInt.test.js
@@ -2,8 +2,20 @@ import { parseInt } from './parseInt';
 
 describe('parseInt', () => {
   it('parseInt does this thing...', () => {
-    const input = null
-    const res = null
+    const input = 'one'
+    const res = 1
+    expect(parseInt(input)).toEqual(res)
+  });
+
+  it('parseInt does this thing...', () => {
+    const input = 'twenty'
+    const res = 20
+    expect(parseInt(input)).toEqual(res)
+  });
+
+  it('parseInt does this thing...', () => {
+    const input = 'two hundred forty-six'
+    const res = 246
     expect(parseInt(input)).toEqual(res)
   });
 });
