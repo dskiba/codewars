c25c800538274edb0989fd6e793270fd95679d3f
Den Skiba <skdenx@gmail.com>
2020-05-24 22:24:34 +0400

    add intervalIntersection solution

--- src/intervalIntersection/intervalIntersection.js
+++ src/intervalIntersection/intervalIntersection.js
@@ -1,3 +1,34 @@
-export const intervalIntersection = () => {
-  // TO IMPLEMENT IN ANOTHER PR
+/**
+ * @param {number[][]} A
+ * @param {number[][]} B
+ * @return {number[][]}
+ */
+export const intervalIntersection = function (A, B) {
+  const res = []
+  for (let i = 0; i < A.length; i++) {
+    const temp = []
+    for (let j = 0; j < B.length; j++) {
+      const [AFrom, ATo] = A[i]
+      const [BFrom, BTo] = B[j]
+      const t = []
+      if (ATo < BFrom) break;
+      if (BFrom >= AFrom && BFrom <= ATo) t.push(BFrom)
+      if (AFrom >= BFrom && AFrom <= BTo) t.push(AFrom)
+      if (ATo >= BFrom && ATo <= BTo) t.push(ATo)
+      if (BTo >= AFrom && BTo <= ATo) t.push(BTo)
+      if (t.length > 0) {
+        const { length } = t
+        if (length > 2) {
+          const r = [t[0], t[length - 1]]
+          temp.push(r)
+        } else {
+          temp.push(t)
+        }
+      }
+    }
+    if (temp.length > 0) {
+      res.push(...temp)
+    }
+  }
+  return res
 };
--- src/intervalIntersection/intervalIntersection.test.js
+++ src/intervalIntersection/intervalIntersection.test.js
@@ -2,8 +2,9 @@ import { intervalIntersection } from './intervalIntersection';
 
 describe('intervalIntersection', () => {
   it('intervalIntersection does this thing...', () => {
-    const input = null
-    const res = null
-    expect(intervalIntersection(input)).toEqual(res)
+    const inputA = [[0, 2], [5, 10], [13, 23], [24, 25]]
+    const inputB = [[1, 5], [8, 12], [15, 24], [25, 26]]
+    const res = [[1, 2], [5, 5], [8, 10], [15, 23], [24, 24], [25, 25]]
+    expect(intervalIntersection(inputA, inputB)).toEqual(res)
   });
 });
