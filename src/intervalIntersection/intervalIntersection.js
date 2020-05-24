0ba47ddf9bc9c77e8d170737bedaf5976e586004
Den Skiba <skdenx@gmail.com>
2020-05-24 22:15:49 +0400

    add intervalIntersection

---
+++ src/intervalIntersection/intervalIntersection.js
@@ -0,0 +1,3 @@
+export const intervalIntersection = () => {
+  // TO IMPLEMENT IN ANOTHER PR
+};
---
+++ src/intervalIntersection/intervalIntersection.test.js
@@ -0,0 +1,9 @@
+import { intervalIntersection } from './intervalIntersection';
+
+describe('intervalIntersection', () => {
+  it('intervalIntersection does this thing...', () => {
+    const input = null
+    const res = null
+    expect(intervalIntersection(input)).toEqual(res)
+  });
+});
