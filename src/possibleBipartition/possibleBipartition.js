9321fc8dd440149df41222942b6a52b54321cabc
Den Skiba <skdenx@gmail.com>
2020-05-27 12:15:48 +0400

    add add possibleBipartition

---
+++ src/possibleBipartition/possibleBipartition.js
@@ -0,0 +1,3 @@
+export const possibleBipartition = () => {
+  // TO IMPLEMENT IN ANOTHER PR
+};
---
+++ src/possibleBipartition/possibleBipartition.test.js
@@ -0,0 +1,9 @@
+import { possibleBipartition } from './possibleBipartition';
+
+describe('possibleBipartition', () => {
+  it('possibleBipartition does this thing...', () => {
+    const input = null
+    const res = null
+    expect(possibleBipartition(input)).toEqual(res)
+  });
+});
